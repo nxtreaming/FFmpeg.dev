18f811d6ff4c1fba47e07fd5ccb4405539419333
GoogleGeek <ffmpeg@gmail.com>
2015-06-15 14:59:04 +0800

    try to enhance HTTP streaming demux

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -4299,6 +4299,7 @@ static AVIndexEntry *mov_find_next_sample(AVFormatContext *s, AVStream **st)
     AVIndexEntry *sample = NULL;
     int64_t best_dts = INT64_MAX;
     int i;
+    int is_http = !av_strncasecmp(s->filename, "http://", 7);
     for (i = 0; i < s->nb_streams; i++) {
         AVStream *avst = s->streams[i];
         MOVStreamContext *msc = avst->priv_data;
@@ -4306,11 +4307,11 @@ static AVIndexEntry *mov_find_next_sample(AVFormatContext *s, AVStream **st)
             AVIndexEntry *current_sample = &avst->index_entries[msc->current_sample];
             int64_t dts = av_rescale(current_sample->timestamp, AV_TIME_BASE, msc->time_scale);
             av_log(s, AV_LOG_TRACE, "stream %d, sample %d, dts %"PRId64"\n", i, msc->current_sample, dts);
-            if (!sample || (!s->pb->seekable && current_sample->pos < sample->pos) ||
+            if (!sample || ((!s->pb->seekable || is_http) && current_sample->pos < sample->pos) ||
                 (s->pb->seekable &&
                  ((msc->pb != s->pb && dts < best_dts) || (msc->pb == s->pb &&
                  ((FFABS(best_dts - dts) <= AV_TIME_BASE && current_sample->pos < sample->pos) ||
-                  (FFABS(best_dts - dts) > AV_TIME_BASE && dts < best_dts)))))) {
+                  (FFABS(best_dts - dts) > AV_TIME_BASE && dts < best_dts && !is_http)))))) {
                 sample = current_sample;
                 best_dts = dts;
                 *st = avst;
